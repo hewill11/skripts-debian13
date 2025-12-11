4200c0397846f4a34879f1cd1fe819fb7fa95895
hewill11 <132582153+hewill11@users.noreply.github.com>
2025-12-11 13:17:49 +0700

    Add files via upload

---
+++ create user.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+useradd -m -s /bin/bash user1
+echo "user1:Password1" | chpasswd
+
+useradd -m -s /bin/bash user2
+echo "user2:Password2" | chpasswd
+
+usermod -aG sudo user2
+
+su - user1
