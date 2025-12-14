9b68e884db43a9b91642c2cfda9979445edad2e9
hewill11 <132582153+hewill11@users.noreply.github.com>
2025-12-15 06:19:01 +0700

    Add files via upload

---
+++ apachenah.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+apt update
+apt install -y apache2
+
+systemctl enable apache2
+systemctl restart apache2
+
+cat > /var/www/html/index.html <<'EOF'
+<html>
+<head>
+    <title>Экзамен_ФАМИЛИЯ СТУДЕНТА</title>
+</head>
+<body>
+    Я на экзамене по МДК 02.01!
+</body>
+</html>
+EOF
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+curl -s http://localhost | head -n 20
