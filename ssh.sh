6051dceba80d13d6ed85052452de4d08fe803786
hewill11 <132582153+hewill11@users.noreply.github.com>
2025-12-17 18:11:53 +0700

    ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -6,11 +6,5 @@ apt install -y openssh-server
 useradd -m student
 echo "student:1" | chpasswd
 
-sed -i 's/^#Port 22/Port 2222/' /etc/ssh/sshd_config
-sed -i 's/^Port 22/Port 2222/' /etc/ssh/sshd_config
-sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
-
 systemctl enable ssh
 systemctl restart ssh
