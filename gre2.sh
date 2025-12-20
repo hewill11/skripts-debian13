9675a9a037cce5b7345fdf1f14690b6fd49c77bd
hewill11 <132582153+hewill11@users.noreply.github.com>
2025-12-20 16:52:06 +0700

    gre2.sh

--- gre2.sh
+++ gre2.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
+set -e
 
-# Очистка старого туннеля (если уже есть)
-ip tunnel del gre1 2>/dev/null
+# cl2: local 192.168.101.10 -> remote 192.168.102.10
+ip tunnel del gre1 2>/dev/null || true
 
-# Создание GRE-туннеля
-ip tunnel add gre1 mode gre \
-local 192.168.102.10 remote 192.168.101.10 ttl 255
-
-# Назначение IP на GRE-интерфейс
-ip addr add 10.10.10.1/30 dev gre1
-
-# Поднятие интерфейса
+ip tunnel add gre1 mode gre local 192.168.101.10 remote 192.168.102.10 ttl 255
+ip addr add 10.10.10.2/30 dev gre1
 ip link set gre1 up
 
-# Маршрут к сети cl2 через GRE
-ip route add 192.168.101.0/24 dev gre1
-
-echo "GRE tunnel on cl1 configured successfully"
+echo "cl2: GRE gre1 поднят (10.10.10.2/30)"
